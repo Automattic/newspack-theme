5c93ce9150693ad15f13e02d678ddb912be04789
Laurel Fulford <laurelfulford@gmail.com>
2019-05-29 20:35:27 -0700

    Update post meta to be closer to the mockups.

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -11,17 +11,8 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php
-		if ( is_sticky() && is_home() && ! is_paged() ) {
-			printf( '<span class="sticky-post">%s</span>', _x( 'Featured', 'post', 'newspack' ) );
-		}
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-		endif;
-		?>
-	</header><!-- .entry-header -->
+		<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
+	</header>
 
 	<?php newspack_post_thumbnail(); ?>
 
