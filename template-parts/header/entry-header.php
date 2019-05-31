151374a692aa6f16b6ae6bc4660c087b5437803f
Laurel Fulford <laurelfulford@gmail.com>
2019-05-31 11:18:43 -0700

    Improve escaping for post and page titles.

--- template-parts/header/entry-header.php
+++ template-parts/header/entry-header.php
@@ -8,13 +8,17 @@
 $discussion = ! is_page() && newspack_can_show_post_thumbnail() ? newspack_get_discussion_data() : null; ?>
 
 
-<?php
-if ( is_singular() ) :
-	the_title( '<h1 class="entry-title">', '</h1>' );
-else :
-	the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-endif;
-?>
+<?php if ( is_singular() ) : ?>
+	<h1 class="entry-title">
+		<?php echo wp_kses_post( get_the_title() ); ?>
+	</h1>
+<?php else : ?>
+	<h2 class="entry-title">
+		<a href="<?php the_permalink(); ?>" rel="bookmark">
+			<?php echo wp_kses_post( get_the_title() ); ?>
+		</a>
+	</h2>
+<?php endif; ?>
 
 <?php if ( ! is_page() ) : ?>
 <div class="entry-meta">
