df9695efde0af500fc41877d7718fcd8da5b6dbe
Philip John <phil@philipjohn.me.uk>
2019-07-04 16:19:39 +0100

    Revert "Remove AMP incompatible script"
    This reverts commit a59c5853809ddbab3239fee52bda2a6d03ca04b2.

--- functions.php
+++ functions.php
@@ -262,6 +262,24 @@ function newspack_scripts() {
 add_action( 'wp_enqueue_scripts', 'newspack_scripts' );
 
 /**
+ * Fix skip link focus in IE11.
+ *
+ * This does not enqueue the script because it is tiny and because it is only for IE11,
+ * thus it does not warrant having an entire dedicated blocking script being loaded.
+ *
+ * @link https://git.io/vWdr2
+ */
+function newspack_skip_link_focus_fix() {
+	// The following is minified via `terser --compress --mangle -- js/skip-link-focus-fix.js`.
+	?>
+	<script>
+	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+	</script>
+	<?php
+}
+add_action( 'wp_print_footer_scripts', 'newspack_skip_link_focus_fix' );
+
+/**
  * Enqueue supplemental block editor styles.
  */
 function newspack_editor_customizer_styles() {
