aba457467bfc1eb3ae58597bf018cc673fbf104a
Laurel Fulford <laurelfulford@gmail.com>
2019-07-04 10:24:12 -0700

    Rough in default styles for the author bio at the bottom of posts.

--- template-parts/post/author-bio.php
+++ template-parts/post/author-bio.php
@@ -26,14 +26,22 @@ if ( (bool) get_the_author_meta( 'description' ) ) : ?>
 	?>
 
 	<div class="author-bio-text">
-		<h2 class="author-title">
-			<?php esc_html( get_the_author() ); ?>
+		<h2 class="accent-header">
+			<?php echo esc_html( get_the_author() ); ?>
+			<span><?php esc_html_e( 'Staff Writer', 'newspack-theme' ); ?></span>
 		</h2>
-		<p class="author-description">
+		<div class="author-meta">
+			<a href="<?php echo 'mailto:' . esc_attr( get_the_author_meta( 'user_email' ) ); ?>"><?php the_author_meta( 'user_email' ); ?></a>
+		</div>
+		<p>
 			<?php the_author_meta( 'description' ); ?>
 		</p><!-- .author-description -->
 		<a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-			<?php esc_html_e( 'View more posts', 'newspack' ); ?>
+			<?php
+				/* translators: %s is the current author's name. */
+				printf( esc_html__( 'More by %s', 'newspack' ), esc_html( get_the_author() ) );
+			?>
 		</a>
+	</div><!-- .author-bio-text -->
 </div><!-- .author-bio -->
 <?php endif; ?>
