0bdba4baece2d32a63bfd2af544250816fd0bb2d
Laurel Fulford <laurel.fulford@automattic.com>
2019-07-23 17:41:17 -0700

    Add 'hamburger' menu icon to SVG icon set.

--- classes/class-newspack-svg-icons.php
+++ classes/class-newspack-svg-icons.php
@@ -171,6 +171,11 @@ class Newspack_SVG_Icons {
     <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
     <path d="M0 0h24v24H0z" fill="none"/>
 </svg>',
+		'menu'                     => /* material-design - menu */ '
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+</svg>',
 
 		'close'                    => /* material-design - close */ '
 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
