bb0c5c08b6e57b2657740a78b0b586248cff9fea
Laurel Fulford <laurel.fulford@automattic.com>
2019-08-05 20:33:10 -0700

    Add file for mobile menu, and a menu icon to the SVG collection.

--- classes/class-newspack-svg-icons.php
+++ classes/class-newspack-svg-icons.php
@@ -177,6 +177,11 @@ class Newspack_SVG_Icons {
     <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
     <path d="M0 0h24v24H0z" fill="none"/>
 </svg>',
+		'menu'                     => /* material-design - menu */ '
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+</svg>',
 
 	);
 
---
+++ template-parts/header/mobile-sidebar.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template for display the AMP mobile navigation.
+ *
+ * @package Newspack
+ */
+?>
+
+<amp-sidebar id="mobile-sidebar" layout="nodisplay" side="right">
+
+	<button class="mobile-menu-toggle" on='tap:mobile-sidebar.toggle'>
+		<?php echo wp_kses( newspack_get_icon_svg( 'close', 20 ), newspack_sanitize_svgs() ); ?>
+		<?php esc_html_e( 'Close', 'newspack' ); ?>
+	</button>
+
+	<?php newspack_tertiary_menu(); ?>
+
+	<?php get_search_form(); ?>
+
+	<?php newspack_primary_menu(); ?>
+
+	<?php newspack_secondary_menu(); ?>
+
+	<?php newspack_social_menu(); ?>
+
+</amp-sidebar>
