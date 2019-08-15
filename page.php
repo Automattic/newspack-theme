612b539894d259254b5e6f7b0e44dd534ffa2324
Laurel Fulford <laurel.fulford@automattic.com>
2019-08-14 22:21:01 -0700

    Pages: Restructure markup to match the single posts.

--- page.php
+++ page.php
@@ -13,24 +13,36 @@ get_header();
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
 
+
 			<?php
 
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
+				?>
 
-				get_template_part( 'template-parts/content/content', 'page' );
+				<header class="entry-header">
+					<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
+				</header>
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
+				<div class="main-content">
 
+					<?php
+					get_template_part( 'template-parts/content/content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
+					?>
+				</div><!-- .main-content -->
+
+			<?php
 			endwhile; // End of the loop.
+			get_sidebar();
 			?>
 
 		</main><!-- #main -->
-		<?php get_sidebar(); ?>
 	</section><!-- #primary -->
 
 <?php
