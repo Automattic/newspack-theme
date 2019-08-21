f5f89bb070f5923fbd08dd6a6c64bf60c5b84476
Laurel Fulford <laurel.fulford@automattic.com>
2019-08-20 20:07:20 -0700

    Update styles and behaviour to include footer widget.

--- template-parts/footer/footer-branding.php
+++ template-parts/footer/footer-branding.php
@@ -5,20 +5,22 @@
  * @package Newspack
  */
 
+$has_footer_logo = false;
+if ( '' !== get_theme_mod( 'newspack_footer_logo', '' ) && 0 !== get_theme_mod( 'newspack_footer_logo', '' ) ) {
+	$has_footer_logo = true;
+}
 
-if ( is_active_sidebar( 'footer-1' ) && has_custom_logo() ) : ?>
+if ( is_active_sidebar( 'footer-1' ) && ( has_custom_logo() || $has_footer_logo ) ) : ?>
 	<div class="footer-branding">
 		<div class="wrapper">
-			<?php
-			if ( '' !== get_theme_mod( 'newspack_footer_logo', '' ) && 0 !== get_theme_mod( 'newspack_footer_logo', '' ) ) :
-			?>
-				<a class="custom-logo-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+			<?php if ( $has_footer_logo ) : ?>
+				<a class="footer-logo-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 					<?php
 					echo wp_get_attachment_image(
 						get_theme_mod( 'newspack_footer_logo', '' ),
 						'newspack-footer-logo',
 						'',
-						array( 'class' => 'custom-logo' )
+						array( 'class' => 'footer-logo' )
 					);
 					?>
 				</a>
