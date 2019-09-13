f51efb369a0acdb071b86de3b7870b6c990a7072
Laurel Fulford <laurel.fulford@automattic.com>
2019-09-13 17:17:37 -0400

    Add check for current template, and change width of 'single wide' template content in editor.

--- functions.php
+++ functions.php
@@ -410,6 +410,18 @@ function newspack_is_active_style_pack() {
 }
 
 /**
+ * Check for specific templates.
+ */
+function newspack_check_current_template() {
+	global $post;
+
+	$post_id       = $post->ID;
+	$template_file = $post_id ? get_post_meta( $post_id, '_wp_page_template', true ) : '';
+
+	return $template_file;
+}
+
+/**
  * Add body class on editor pages if editing the static front page.
  */
 function newspack_filter_admin_body_class( $classes ) {
@@ -420,6 +432,10 @@ function newspack_filter_admin_body_class( $classes ) {
 
 	if ( 'default' !== get_theme_mod( 'active_style_pack', 'default' ) ) {
 		$classes .= ' style-pack-' . get_theme_mod( 'active_style_pack', 'default' );
+	}
+
+	if ( 'single-wide.php' === newspack_check_current_template() ) {
+		$classes .= ' newspack-single-wide-template';
 	}
 
 	return $classes;
