1ab351e0d5f92cb04712755ed0652cf584b7285d
Thomas Guillot <thomasguillot@users.noreply.github.com>
2019-10-09 16:12:10 +0100

    Introduce `newspack_get_primary_color_variation()` to get the exact Primary Variation and not via `newspack_adjust_brightness()`

--- inc/color-filters.php
+++ inc/color-filters.php
@@ -9,6 +9,7 @@
  * Define default color filters.
  */
 define( 'NEWSPACK_DEFAULT_PRIMARY', '#3366ff' ); // Hex
+define( 'NEWSPACK_DEFAULT_PRIMARY_VARIATION', '#2240d5' ); // Hex
 define( 'NEWSPACK_DEFAULT_SECONDARY', '#666666' ); // Hex
 
 /**
@@ -27,6 +28,24 @@ function newspack_get_primary_color() {
 	 * @param string $value Sets a hexidecimal color; uses theme default defined above.
 	 */
 	return apply_filters( 'newspack_primary_color', NEWSPACK_DEFAULT_PRIMARY );
+}
+
+/**
+ * The default color used for the primary color variation throughout this theme
+ *
+ * @return string the default hexidecimal color.
+ */
+function newspack_get_primary_color_variation() {
+	/**
+	 * Default primary color variation.
+	 *
+	 * Sets the default primary color variation for the theme's custom colors.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $value Sets a hexidecimal color; uses theme default defined above.
+	 */
+	return apply_filters( 'newspack_primary_color_variation', NEWSPACK_DEFAULT_PRIMARY_VARIATION );
 }
 
 /**
