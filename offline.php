0fc3f3e262eb9dfaacc48c23372c0001e5a0cd85
laurelfulford <laurelfulford@gmail.com>
2019-10-17 22:01:17 +0000

    Clean up offline header.

--- offline.php
+++ offline.php
@@ -1,13 +1,6 @@
 <?php
 /**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * The PWA Offline template file
  *
  * @package Newspack
  */
@@ -15,6 +8,9 @@
 get_header();
 ?>
 	<section id="primary" class="content-area">
+		<header class="entry-header">
+			<h1 class="entry-title"><?php esc_html_e( 'Error', 'newspack' ); ?></h1>
+		</header><!-- .entry-header -->
 		<main id="main" class="site-main">
 			<?php wp_service_worker_error_message_placeholder(); ?>
 		</main><!-- .site-main -->
