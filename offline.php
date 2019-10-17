dcd01a19a36924c14ea4c986b7ef03534df050e3
laurelfulford <laurelfulford@gmail.com>
2019-10-17 22:17:53 +0000

    Update messages; add function_exists check.

--- offline.php
+++ offline.php
@@ -9,13 +9,16 @@ get_header();
 ?>
 	<section id="primary" class="content-area">
 		<header class="entry-header">
-			<h1 class="entry-title"><?php esc_html_e( 'Error', 'newspack' ); ?></h1>
+			<h1 class="entry-title"><?php esc_html_e( 'Oops! It looks like you&rsquo;re offline.', 'newspack' ); ?></h1>
 		</header><!-- .entry-header -->
 		<main id="main" class="site-main">
-			<?php wp_service_worker_error_message_placeholder(); ?>
+			<?php
+			if ( function_exists( 'wp_service_worker_error_message_placeholder' ) ) {
+				wp_service_worker_error_message_placeholder();
+			}
+			?>
 		</main><!-- .site-main -->
 	</section><!-- .content-area -->
-
 <?php
 get_footer();
 
