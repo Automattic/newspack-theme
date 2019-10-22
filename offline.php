f92f9c1ea67ed575111efc98fda280919709b931
laurelfulford <laurelfulford@gmail.com>
2019-10-22 20:43:09 +0000

    Update title on PWA offline template to be more accurate.

--- offline.php
+++ offline.php
@@ -9,7 +9,7 @@ get_header();
 ?>
 	<section id="primary" class="content-area">
 		<header class="entry-header">
-			<h1 class="entry-title"><?php esc_html_e( 'Oops! It looks like you&rsquo;re offline.', 'newspack' ); ?></h1>
+			<h1 class="entry-title"><?php esc_html_e( 'Offline', 'newspack' ); ?></h1>
 		</header><!-- .entry-header -->
 		<main id="main" class="site-main">
 			<?php
