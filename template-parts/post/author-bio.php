f4262b30410bf35417e3063f479064f921a5dc3c
Laurel Fulford <laurel.fulford@automattic.com>
2019-11-06 15:51:11 -0800

    Add Co-author support to author bio template.

--- template-parts/post/author-bio.php
+++ template-parts/post/author-bio.php
@@ -5,7 +5,70 @@
  * @package Newspack
  */
 
-if ( (bool) get_the_author_meta( 'description' ) && is_single() ) : ?>
+
+if ( function_exists( 'coauthors_posts_links' ) && is_single() ) :
+
+	$authors      = get_coauthors();
+	$author_count = count( $authors );
+	$i            = 1;
+
+	foreach ( $authors as $author ) {
+
+		if ( '' !== $author->description ) { ?>
+
+			<div class="author-bio">
+				<?php
+				if ( ! newspack_is_active_style_pack( 'style-4' ) ) {
+					$author_avatar = coauthors_get_avatar( $author, 80 );
+					if ( $author_avatar ) {
+						// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+						echo $author_avatar;
+					}
+				}
+				?>
+
+				<div class="author-bio-text">
+					<div class="author-bio-header">
+						<?php
+						if ( newspack_is_active_style_pack( 'style-4' ) ) {
+							$author_avatar = coauthors_get_avatar( $author, 80 );
+							if ( $author_avatar ) {
+								// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+								echo $author_avatar;
+							}
+						}
+						?>
+
+						<div>
+							<h2 class="accent-header">
+								<?php echo esc_html( esc_html( $author->display_name ) ); ?>
+								<span><?php // TODO: Add Job title ?></span>
+							</h2>
+							<div class="author-meta">
+								<a class="author-email" href="<?php echo 'mailto:' . esc_attr( $author->user_email ); ?>"><?php echo esc_html( $author->user_email ); ?></a>
+							</div>
+						</div>
+					</div><!-- .author-bio-header -->
+
+					<p>
+						<?php echo wp_kses_post( $author->description ); ?>
+					</p><!-- .author-description -->
+					<a class="author-link" href="<?php echo esc_url( get_author_posts_url( $author->ID, $author->user_nicename ) ); ?>" rel="author">
+						<?php
+							/* translators: %s is the current author's name. */
+							printf( esc_html__( 'More by %s', 'newspack' ), esc_html( $author->display_name ) );
+						?>
+					</a>
+				</div><!-- .author-bio-text -->
+
+			</div><!-- .author-bio -->
+		<?php
+		}
+	}
+
+elseif ( (bool) get_the_author_meta( 'description' ) && is_single() ) :
+?>
+
 <div class="author-bio">
 
 	<?php
