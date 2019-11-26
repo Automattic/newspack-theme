6efd51fa0c48e0908ddd14ee396dc7f324127f59
Laurel Fulford <laurel.fulford@automattic.com>
2019-11-25 16:44:30 -0800

    Fix issue with tagline not inheriting custom fonts.

--- inc/typography.php
+++ inc/typography.php
@@ -140,6 +140,7 @@ function newspack_custom_typography_css() {
 
 		if ( newspack_is_active_style_pack( 'style-4' ) ) {
 			$css_blocks .= "
+			.site-description,
 			.has-drop-cap:not(:focus)::first-letter,
 			.taxonomy-description,
 			.entry .entry-content blockquote, .entry .entry-content blockquote cite, .entry .entry-content .wp-block-pullquote cite {
