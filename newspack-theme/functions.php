91e2f196350528df4579c745883c4061b2160d53
Laurel Fulford <laurel.fulford@automattic.com>
2020-06-02 10:47:55 -0700

    fix: correct theme content width

--- newspack-theme/functions.php
+++ newspack-theme/functions.php
@@ -324,7 +324,7 @@ function newspack_content_width() {
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'newspack_content_width', 960 );
+	$GLOBALS['content_width'] = apply_filters( 'newspack_content_width', 1200 );
 }
 add_action( 'after_setup_theme', 'newspack_content_width', 0 );
 
