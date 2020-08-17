daadd174367634ba51db0116e79c858605aa16b6
Laurel Fulford <laurel.fulford@automattic.com>
2020-08-17 15:00:52 -0700

    fix: prevent JavaScript error in sponsor AMP-fallback code

--- newspack-theme/js/src/amp-fallback-newspack-sponsors.js
+++ newspack-theme/js/src/amp-fallback-newspack-sponsors.js
@@ -6,28 +6,31 @@
 
 ( function() {
 	// Support info toggle.
-	const supportToggle = document.getElementById( 'sponsor-info-toggle' ),
-		supportLabel = supportToggle.parentNode,
-		supportInfo = document.getElementById( 'sponsor-info' ),
-		supportToggleTextContain = supportToggle.getElementsByTagName( 'span' )[ 0 ],
-		supportToggleTextDefault = supportToggleTextContain.innerText;
+	const supportToggle = document.getElementById( 'sponsor-info-toggle' );
 
-	supportToggle.addEventListener(
-		'click',
-		function() {
-			supportLabel.classList.toggle( 'show-info' );
-			// Toggle screen reader text label and aria settings.
-			if ( supportToggleTextDefault === supportToggleTextContain.innerText ) {
-				supportToggleTextContain.innerText = newspackScreenReaderText.close_info;
+	if ( null !== supportToggle ) {
+		const supportLabel = supportToggle.parentNode,
+			supportInfo = document.getElementById( 'sponsor-info' ),
+			supportToggleTextContain = supportToggle.getElementsByTagName( 'span' )[ 0 ],
+			supportToggleTextDefault = supportToggleTextContain.innerText;
 
-				supportInfo.setAttribute( 'aria-expanded', 'true' );
-				supportToggle.setAttribute( 'aria-expanded', 'true' );
-			} else {
-				supportToggleTextContain.innerText = supportToggleTextDefault;
-				supportInfo.setAttribute( 'aria-expanded', 'false' );
-				supportToggle.setAttribute( 'aria-expanded', 'false' );
-			}
-		},
-		false
-	);
+		supportToggle.addEventListener(
+			'click',
+			function() {
+				supportLabel.classList.toggle( 'show-info' );
+				// Toggle screen reader text label and aria settings.
+				if ( supportToggleTextDefault === supportToggleTextContain.innerText ) {
+					supportToggleTextContain.innerText = newspackScreenReaderText.close_info;
+
+					supportInfo.setAttribute( 'aria-expanded', 'true' );
+					supportToggle.setAttribute( 'aria-expanded', 'true' );
+				} else {
+					supportToggleTextContain.innerText = supportToggleTextDefault;
+					supportInfo.setAttribute( 'aria-expanded', 'false' );
+					supportToggle.setAttribute( 'aria-expanded', 'false' );
+				}
+			},
+			false
+		);
+	}
 } )();
