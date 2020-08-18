40167a1d16188aca990098d05bcf93a422b153ee
Laurel Fulford <laurel.fulford@automattic.com>
2020-08-18 15:34:40 -0700

    fix: correct logic for when top menu area should be shown

--- newspack-theme/header.php
+++ newspack-theme/header.php
@@ -74,7 +74,7 @@ endif;
 				</div>
 			</div><!-- .wrapper -->
 		<?php else : ?>
-			<?php if ( has_nav_menu( 'secondary-menu' ) || ( true === $show_slideout_sidebar && false === $header_simplified ) ) : ?>
+			<?php if ( has_nav_menu( 'secondary-menu' ) ) : ?>
 				<div class="top-header-contain desktop-only">
 					<div class="wrapper">
 						<?php if ( true === $show_slideout_sidebar && has_nav_menu( 'secondary-menu' ) ) : ?>
