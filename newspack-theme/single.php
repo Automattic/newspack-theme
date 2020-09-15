2ff441603f0b0ccab4b5e578302da33676d1c311
Laurel <laurel.fulford@automattic.com>
2020-09-15 03:49:32 -0700

    feat: support aside post format (#1074)
    * feat: add aside post format
    
    * feat: adjust font sizes for post formats
    
    * feat: hide bylines in archive and search
    
    * feat: hide bylines from single posts
    
    * fix: make sure avatars are hidden for asides in the editor

--- newspack-theme/single.php
+++ newspack-theme/single.php
@@ -41,7 +41,7 @@ get_header();
 						newspack_post_thumbnail();
 					endif;
 
-					get_template_part( 'template-parts/content/content', 'single' );
+					get_template_part( 'template-parts/content/content-single', 'single' );
 
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) {
