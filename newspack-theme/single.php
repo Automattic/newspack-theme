47583907c3fe4c3d1f6eb78a403739b22b775d20
Laurel Fulford <laurel.fulford@automattic.com>
2021-01-12 17:37:07 -0800

    feat: add previous and next links to single posts

--- newspack-theme/single.php
+++ newspack-theme/single.php
@@ -43,6 +43,10 @@ get_header();
 
 					get_template_part( 'template-parts/content/content-single', 'single' );
 
+					if ( is_singular( 'post' ) ) {
+						newspack_previous_next();
+					}
+
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) {
 						newspack_comments_template();
