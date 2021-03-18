43ed8754aebf9e47da3f80f5ef1a6dd34ea34d7b
Laurel <laurel.fulford@automattic.com>
2021-03-18 01:21:21 -0700

    feat: add option to set footer logo size (#1302)
    * feat: add option to set footer logo size
    
    * feat: add 2 more sizes, XS and XL
    
    * feat: allow footer logo to skip cropping

--- newspack-theme/functions.php
+++ newspack-theme/functions.php
@@ -65,7 +65,7 @@ if ( ! function_exists( 'newspack_setup' ) ) :
 		add_image_size( 'newspack-featured-image', 1200, 9999 );
 		add_image_size( 'newspack-archive-image', 800, 600, true );
 		add_image_size( 'newspack-archive-image-large', 1200, 900, true );
-		add_image_size( 'newspack-footer-logo', 400, 9999, true );
+		add_image_size( 'newspack-footer-logo', 400, 9999 );
 
 		// This theme uses wp_nav_menu() in two locations.
 		register_nav_menus(
