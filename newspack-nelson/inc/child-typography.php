31f47ee1c7d3da1503d7a71deb734afa8df68a17
9ete <pete@petelower.com>
2021-11-29 17:41:09 -0800

    fix/1470: newspack-nelson: add .widgettitle instance where all instances of .accent-header and .widget-title exist to resolve issue caused by gutenburg bug

--- newspack-nelson/inc/child-typography.php
+++ newspack-nelson/inc/child-typography.php
@@ -37,19 +37,23 @@ function newspack_nelson_custom_typography_css() {
 			.nav3,
 			.mobile-menu-toggle,
 			.accent-header,
+			.widgettitle,
 			.cat-links,
 			div.wpnbha .article-section-title,
 			.entry-meta .byline .author,
 			.tags-links a,
 			.post-edit-link,
 			.author-bio h2 span,
-			.site-footer .widget-title {
+			.site-footer .widget-title ,
+			.site-footer .widgettitle {
 				letter-spacing: 0.05em;
 				text-transform: uppercase;
 			}
 		';
 		$editor_css_blocks .= '
 			.block-editor-block-list__layout .block-editor-block-list__block.accent-header,
+			.block-editor-block-list__layout .block-editor-block-list__block.widget-title,
+			.block-editor-block-list__layout .block-editor-block-list__block.widgettitle,
 			.block-editor-block-list__layout .block-editor-block-list__block .article-section-title,
 			.block-editor-block-list__layout .block-editor-block-list__block .entry-meta .byline a,
 			.block-editor-block-list__layout .block-editor-block-list__block .cat-links {
