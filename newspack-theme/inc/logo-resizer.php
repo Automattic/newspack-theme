34623e07f389aed70cc6a3f02c207ee2dfdc3a93
Laurel Fulford <laurel.fulford@automattic.com>
2022-01-26 16:16:40 -0800

    fix: add width: auto to the sticky header logo

--- newspack-theme/inc/logo-resizer.php
+++ newspack-theme/inc/logo-resizer.php
@@ -139,6 +139,7 @@ function newspack_customize_logo_resize( $html ) {
 		@media (max-width: 1199px) and (min-width: 782px) {
 			.h-stk.h-cl:not(.h-sub) .site-header .custom-logo {
 				max-width: 100%;
+				width: auto;
 			}
 		}
 
