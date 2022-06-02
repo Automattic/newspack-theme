11846998f672a157a04e3e56c681404d3f6b10ce
Laurel Fulford <laurel.fulford@automattic.com>
2022-06-02 15:38:44 -0700

    feat: add title, featured image output

--- newspack-theme/blank.php
+++ newspack-theme/blank.php
@@ -27,13 +27,36 @@
 		<section id="primary" class="content-area">
 			<main id="main" class="site-main">
 				<?php
+
 				/* Start the Loop */
 				while ( have_posts() ) :
 					the_post();
+
+					// Template part for large featured images.
+					if ( in_array( newspack_featured_image_position(), array( 'large', 'behind', 'beside', 'above' ) ) ) :
+						get_template_part( 'template-parts/post/large-featured-image' );
+					else :
 					?>
+						<header class="entry-header">
+							<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
+						</header>
+					<?php endif; ?>
 
 					<div class="main-content">
-						<?php get_template_part( 'template-parts/content/content', 'page' ); ?>
+
+						<?php
+						// Place smaller featured images inside of 'content' area.
+						if ( 'small' === newspack_featured_image_position() ) {
+							newspack_post_thumbnail();
+						}
+
+						get_template_part( 'template-parts/content/content', 'page' );
+
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) {
+							newspack_comments_template();
+						}
+						?>
 					</div>
 
 				<?php endwhile; ?>
