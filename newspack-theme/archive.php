7f9dcab3bd21580b0889ad2e125024f9bd896645
Laurel Fulford <laurel.fulford@automattic.com>
2022-10-03 14:57:12 -0700

    feat: experiment with template parts

--- newspack-theme/archive.php
+++ newspack-theme/archive.php
@@ -119,35 +119,8 @@ $show_excerpt        = get_theme_mod( 'archive_show_excerpt', false );
 
 		<main id="main" class="site-main">
 
-		<?php
-		if ( have_posts() ) :
-			$post_count = 0;
+		<?php block_template_part( 'archive-content' ); ?>
 
-			// Start the Loop.
-			while ( have_posts() ) :
-				$post_count++;
-				the_post();
-
-				// Check if you're on the first post of the first page and if it should be styled differently, or if excerpts are enabled.
-				if ( ( 1 === $post_count && 0 === get_query_var( 'paged' ) && true === $feature_latest_post ) || true === $show_excerpt ) {
-					get_template_part( 'template-parts/content/content', 'excerpt' );
-				} else {
-					get_template_part( 'template-parts/content/content', 'archive' );
-				}
-
-				do_action( 'after_archive_post', $post_count );
-				// End the loop.
-			endwhile;
-
-			// Previous/next page navigation.
-			newspack_the_posts_navigation();
-
-			// If no content, include the "No posts found" template.
-		else :
-			get_template_part( 'template-parts/content/content', 'none' );
-
-		endif;
-		?>
 		</main><!-- #main -->
 		<?php
 		$archive_layout = get_theme_mod( 'archive_layout', 'default' );
