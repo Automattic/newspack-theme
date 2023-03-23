c94d86a72b959f94693f7b2af663c80f6f68d940
Laurel Fulford <laurel.fulford@automattic.com>
2023-03-22 18:56:48 -0700

    chore: remove unneeded code; correct citation styles

--- newspack-joseph/inc/child-typography.php
+++ newspack-joseph/inc/child-typography.php
@@ -8,11 +8,40 @@
  * Generate the CSS for custom typography.
  */
 function newspack_joseph_custom_typography_css() {
-	$font_body   = newspack_font_stack( get_theme_mod( 'font_body', '' ), get_theme_mod( 'font_body_stack', 'serif' ) );
 	$font_header = newspack_font_stack( get_theme_mod( 'font_header', '' ), get_theme_mod( 'font_header_stack', 'serif' ) );
 
 	$css_blocks        = '';
 	$editor_css_blocks = '';
+
+	if ( '' !== $font_header ) {
+		$css_blocks .= '
+			/* Font Family */
+			figcaption,
+			.entry-meta,
+			.cat-links,
+			.entry-footer,
+			.nav1,
+			.secondary-menu,
+			.nav3,
+			.site-description,
+			.site-info,
+			#cancel-comment-reply-link,
+			#mobile-sidebar,
+			.widget,
+			.widget-title.accent-header,
+			.widgettitle,
+			.wp-block-button__link,
+			div.wp-block-file,
+			div.wp-block-file .wp-block-file__button,
+			.wp-block-pullquote cite,
+			button,
+			input[type="button"],
+			input[type="reset"],
+			input[type="submit"] {
+				font-family: var( --newspack-theme-font-heading );
+			}
+		';
+	}
 
 	if ( true === get_theme_mod( 'accent_allcaps', true ) ) {
 		$css_blocks        .= '
