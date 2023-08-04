bab33a2cc9dc8f07dfbc0551d90b9d27488d198d
Leo Germani <leogermani@automattic.com>
2023-08-03 23:24:38 -0300

    feat: AI subtitle suggestions poc

--- newspack-theme/functions.php
+++ newspack-theme/functions.php
@@ -1334,3 +1334,5 @@ if ( class_exists( 'Tribe__Events__Main' ) ) {
 if ( class_exists( 'Newspack_Multibranded_Site\Customizations\Theme_Colors' ) ) {
 	require get_template_directory() . '/inc/newspack-multibranded-site-plugin.php';
 }
+
+require get_template_directory() . '/inc/newspack-gpt.php';
--- newspack-theme/js/src/post-subtitle/SubtitleEditor.js
+++ newspack-theme/js/src/post-subtitle/SubtitleEditor.js
@@ -1,10 +1,14 @@
 /**
  * WordPress dependencies
  */
+import apiFetch from '@wordpress/api-fetch';
+const { getCurrentPostId } = wp.data.select( 'core/editor' );
 import { compose } from '@wordpress/compose';
 import { withDispatch } from '@wordpress/data';
 import { useState, useEffect } from '@wordpress/element';
-import { TextareaControl } from '@wordpress/components';
+import { TextareaControl, Button } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { addQueryArgs } from '@wordpress/url';
 
 /**
  * Internal dependencies
@@ -26,17 +30,38 @@ const decorate = compose(
 
 const SubtitleEditor = ( { subtitle, saveSubtitle } ) => {
 	const [ value, setValue ] = useState( subtitle );
+	const [ isLoading, setIsLoading ] = useState( false );
 
 	useEffect( () => {
 		saveSubtitle( value );
 	}, [ value ] );
 
+	const requestSubtitleSuggestion = () => {
+		setIsLoading( true );
+		apiFetch( {
+			path: addQueryArgs( '/newspack-theme-gpt/v1/get-subtitle', { post_id: getCurrentPostId() } ),
+		} )
+			.then( suggestion => {
+				setValue( suggestion );
+				setIsLoading( false );
+			} )
+			.finally( () => {
+				setIsLoading( false );
+			} );
+	};
+
 	return (
-		<TextareaControl
-			value={ value }
-			onChange={ setValue }
-			style={ { marginTop: '10px', width: '100%' } }
-		/>
+		<>
+			<TextareaControl
+				value={ value }
+				onChange={ setValue }
+				style={ { marginTop: '10px', width: '100%' } }
+				disabled={ isLoading }
+			/>
+			<Button variant="secondary" onClick={ requestSubtitleSuggestion } disabled={ isLoading }>
+				{ __( 'Get subtitle suggestion', 'newspack-theme' ) }
+			</Button>
+		</>
 	);
 };
 
