b2dbc607fdcbb70952a186b71ac6b7b81d4b994d
Laurel <laurel.fulford@automattic.com>
2024-01-22 14:51:35 -0800

    fix: replace contrast-related colours code with CSS variables (#2109)

--- newspack-katharine/inc/child-color-patterns.php
+++ newspack-katharine/inc/child-color-patterns.php
@@ -8,14 +8,9 @@
  * Add child theme-specific custom colours.
  */
 function newspack_katharine_custom_colors_css() {
-	$primary_color   = newspack_get_primary_color();
-	$secondary_color = newspack_get_secondary_color();
 	$header_color    = '#333';
 
 	if ( 'default' !== get_theme_mod( 'theme_colors', 'default' ) ) {
-		$primary_color   = get_theme_mod( 'primary_color_hex', $primary_color );
-		$secondary_color = get_theme_mod( 'secondary_color_hex', $secondary_color );
-
 		if ( 'default' !== get_theme_mod( 'header_color', 'default' ) ) {
 			$header_color = get_theme_mod( 'header_color_hex', '#666666' );
 		}
@@ -26,58 +21,13 @@ function newspack_katharine_custom_colors_css() {
 		}
 	}
 
+	$theme_css  = '';
+	$editor_css = '';
+
 	// Set colour contrasts.
-	$primary_color_contrast   = newspack_get_color_contrast( $primary_color );
-	$secondary_color_contrast = newspack_get_color_contrast( $secondary_color );
 	$header_color_contrast    = newspack_get_color_contrast( $header_color );
 
-	$theme_css = '
-		.archive .page-title,
-		.entry-meta .byline a,
-		.entry-meta .byline a:visited,
-		.cat-links,
-		.cat-links a,
-		.cat-links a:visited,
-		.article-section-title,
-		.entry .entry-footer,
-		.accent-header,
-		#secondary .widgettitle {
-			color: ' . esc_html( newspack_color_with_contrast( $primary_color ) ) . ';
-		}
-
-		.mobile-sidebar .accent-header:before,
-		.mobile-sidebar div.wpnbha .article-section-title:before,
-		.mobile-sidebar .cat-links:before,
-		.mobile-sidebar figcaption:after,
-		.mobile-sidebar .wp-caption-text:after {
-			background-color: ' . esc_html( $primary_color_contrast ) . ';
-		}
-
-		@media only screen and (min-width: 782px) {
-			.featured-image-beside a,
-			.featured-image-beside a:visited,
-			.featured-image-beside .cat-links a {
-				color: ' . esc_html( $primary_color_contrast ) . ';
-			}
-
-			.featured-image-beside .cat-links:before {
-				background-color: ' . esc_html( $primary_color_contrast ) . ';
-			}
-		}
-	';
-
 	if ( true === get_theme_mod( 'header_solid_background', false ) ) {
-		$theme_css .= '
-			/* Featured Image Beside styles */
-			@media only screen and (min-width: 782px) {
-				.h-sb .featured-image-beside,
-				.h-sb .featured-image-beside a {
-					color: ' . esc_html( $primary_color_contrast ) . ';
-				}
-			}
-		';
-
-
 		if ( 'default' !== get_theme_mod( 'header_color', 'default' ) ) {
 			$theme_css .= '
 				/* Header solid background */
@@ -124,14 +74,6 @@ function newspack_katharine_custom_colors_css() {
 			}
 		';
 	}
-
-	$editor_css = '
-		.block-editor-block-list__layout .block-editor-block-list__block .entry-meta .byline a,
-		.block-editor-block-list__layout .block-editor-block-list__block.accent-header,
-		.block-editor-block-list__layout .block-editor-block-list__block .wp-block-newspack-blocks-homepage-articles:not(.has-text-color) .article-section-title {
-			color: ' . esc_html( newspack_color_with_contrast( $primary_color ) ) . ';
-		}
-	';
 
 	if ( function_exists( 'register_block_type' ) && is_admin() ) {
 		$theme_css = $editor_css;
