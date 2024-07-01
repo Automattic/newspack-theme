e8365e88615c7dc0c8f46436a097b3dc986116ab
dkoo <derrick.koo@automattic.com>
2024-07-01 12:24:51 -0600

    fix: post-release job for Node v16

--- scripts/create-child-releases.js
+++ scripts/create-child-releases.js
@@ -3,6 +3,7 @@
 const { basename, resolve } = require( 'path' );
 const { stat, readFile } = require( 'fs-extra' );
 const { Octokit } = require( '@octokit/rest' );
+require( 'node-fetch' ); // Required for Node v16. See: https://github.com/octokit/octokit.js/#fetch-missing
 
 const THEMES = [
 	'newspack-theme',
